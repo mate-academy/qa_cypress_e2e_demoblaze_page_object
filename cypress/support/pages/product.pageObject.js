fc4149cda09296da9ec2f982d2a1988fe5e27db8
Oleksii Shypovalov <asipovalov9@gmail.com>
2023-06-07 13:37:17 +0300

    Solution1.1

---
+++ cypress/support/pages/product.pageObject.js
@@ -0,0 +1,17 @@
+import PageObject from '../PageObject';
+
+class ProductPageObject extends PageObject {
+  clickOnAddBtn() {
+    cy.contains('.btn', 'Add to cart')
+      .click();
+  }
+
+  assertAllert(assertText) {
+    cy.on('window:alert',(t)=>{
+      expect(t).to.contains(assertText);
+   })
+  }
+
+}
+
+export default ProductPageObject;
