dbdaab74f075ed70f55e3a1134448e0f51bb9f8d
BakumSV <immigrantsong61@gmail.com>
2023-06-07 18:08:51 +0300

    Task is resolved

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,49 @@
+import OrderFormPageObject from '../support/pages/orderForm.pageObject';
+import HomeAndCataloguePageObject from '../support/pages/homeСatalogue.pageObject';
+import faker from 'faker';
 /// <reference types="cypress" />
 
-describe('', () => {
-  before(() => {
+const orderForm = new OrderFormPageObject();
+const homePage = new HomeAndCataloguePageObject();
 
+const orderData = {
+  name: faker.name.firstName(),
+  country: faker.random.word(),
+  city: faker.random.word(),
+  creditCard: '1111-1111-1111-1111',
+  month: faker.random.word(),
+  year: 2023,
+  successMessage: 'Thank you for your purchase!'
+};
+
+describe('Users purchasing flow', () => {
+  before(() => {
+    homePage.visit();
   });
 
-  it('', () => {
+  it('should provide the ability to ...', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    homePage.clickOnBtn('Add to cart');
+    homePage.clickOnLink('Cart');
+    homePage.assertAllert('Product added');
+    homePage.clickOnBtn('Place Order');
 
+    orderForm.nameField
+      .type(orderData.name);
+    orderForm.countryField
+      .type(orderData.country);
+    orderForm.cityField
+      .type(orderData.city);
+    orderForm.creditCardField
+      .type(orderData.creditCard);
+    orderForm.monthField
+      .type(orderData.month);
+    orderForm.yearField
+      .type(orderData.year);
+    homePage.clickOnBtn('Purchase');
+
+    orderForm.confirmBtn
+      .click();
   });
 });
