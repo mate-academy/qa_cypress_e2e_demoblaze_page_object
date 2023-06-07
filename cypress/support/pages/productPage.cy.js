ebbcf01d04cb4eebd772939d6f74af6d4be43ff4
YuryTatarko <hatemaders@gmail.com>
2023-06-07 19:56:06 +0300

    Demoblaze code

---
+++ cypress/support/pages/productPage.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+class ProductPage {
+    clickOnProduct(productName) {
+      cy.contains(productName).click();
+    }
+  
+    clickAddToCart() {
+      cy.contains('Add to cart').click();
+    }
+  
+    verifyAlert() {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal('Product added');
+      });
+    }
+  }
+  
+  export default ProductPage;
+  
