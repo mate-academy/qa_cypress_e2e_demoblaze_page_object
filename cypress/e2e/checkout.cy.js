5f9d5c7a7775decfacfbe100f8377eedd649bb75
Dmytro <dmytrywork81@gmail.com>
2023-06-07 18:40:32 +0300

    fixed

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,10 +1,16 @@
-import HomeAndCataloguePageObject from '../support/pages/homeСatalogue.pageObject';
 import faker from 'faker';
+import HomePage from '../support/pages/homePage.сy';
+import ProductPage from '../support/pages/productPage.cy';
+import CartPage from '../support/pages/cartPage.cy';
+import PlaceOrderPage from '../support/pages/placeOrderPage.cy';
 
 /// <reference types="cypress" />
 
 describe('Order - User is able to order the product', () => {
-  const homePage = new HomeAndCataloguePageObject();
+  const homePage = new HomePage();
+  const productPage = new ProductPage();
+  const cartPage = new CartPage();
+  const placeOrderPage = new PlaceOrderPage();
 
   beforeEach(() => {
     cy.viewport(1000, 600);
@@ -25,26 +31,19 @@ describe('Order - User is able to order the product', () => {
     };
 
     homePage.clickOnCategory(testData.category);
-    homePage.clickOnProduct(testData.product);
-    cy.contains('Add to cart').click();
-    cy.window().then((win) => {
-      cy.stub(win, 'alert').as('alertStub');
-    });
-    cy.get('@alertStub').should('be.called');
-    homePage.clickOnLink('Cart');
-    cy.get('.col-lg-8').should('contain', testData.product);
-    cy.contains('Place Order').click();
-    cy.get('#name').type(testData.name);
-    cy.get('#country').type(testData.country);
-    cy.get('#city').type(testData.city);
-    cy.get('#card').type(testData.cardNumber);
-    cy.get('#month').type(testData.month);
-    cy.get('#year').type(testData.year.toString());
-    cy.contains('Purchase').click();
-    cy.get('.sweet-alert').should('contain', testData.successMessage);
-    cy.get('.lead').should('contain', testData.name);
-    cy.get('.lead').should('contain', testData.cardNumber);
-    cy.contains('OK').click();
+    productPage.clickOnProduct(testData.product);
+    productPage.clickAddToCart();
+    productPage.verifyAlert();
+
+    cartPage.clickOnLink('Cart');
+    cartPage.verifyProduct(testData.product);
+
+    cartPage.clickOnLink('Place Order');
+    placeOrderPage.fillOrderForm(testData);
+    placeOrderPage.clickPurchase();
+    placeOrderPage.verifySuccessMessage(testData.successMessage);
+    placeOrderPage.verifyOrderDetails(testData.name, testData.cardNumber);
+    cy.get('.confirm').click();
     cy.visit('https://www.demoblaze.com/index.html');
   });
 });
