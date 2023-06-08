7729ed53c137a0fe807b3d1c7c31f67bdc40a2e3
Olga Akimenko <olga.katsion@gmail.com>
2023-06-08 14:02:21 +0300

    Solution

--- cypress/support/PageObject.js
+++ cypress/support/PageObject.js
@@ -1,13 +1,61 @@
-class PageObject {
-  visit(url) {
-    cy.visit(url || this.url);
-  }
+import PageObject from '../PageObject';
 
-  assertAllert(alertMessage) {
-    cy.on('window:alert', (alert) => {
-      expect(alert).to.eq(alertMessage);
-    });
-  }
+class CartPageObject extends PageObject {
+assertProductInCart(productName) {
+cy.get('.table-responsive')
+.should('contain', productName);
 }
 
-export default PageObject;
+clickOnPlaceOrderBtn() {
+cy.contains('.btn', 'Place Order')
+.click();
+}
+
+typeName(name) {
+cy.get('#name')
+.type(name);
+}
+
+typeCountry(country) {
+cy.get('#country')
+.type(country);
+}
+
+typeCity(city) {
+cy.get('#city')
+.type(city);
+}
+
+typeCreditCard(cardNumber) {
+cy.get('#card')
+.type(cardNumber);
+}
+
+typeMonth(month) {
+cy.get('#month')
+.type(month);
+}
+
+typeYear(year) {
+cy.get('#year')
+.type(year);
+}
+
+clickOnPurchaseBtn() {
+cy.contains('.btn', 'Purchase')
+.click();
+}
+
+assertDataInModal(cardNumber, firstName) {
+cy.get('.sweet-alert')
+.should('contain', `Card Number: ${cardNumber}`)
+.and('contain', `Name: ${firstName}`);
+}
+
+clickOnOkBtn() {
+cy.contains('.confirm.btn', 'OK')
+.click();
+}
+}
+
+export default CartPageObject;
