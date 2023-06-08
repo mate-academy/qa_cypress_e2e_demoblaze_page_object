4b185b66ff61e064398e5648c5ff9d0babc901fc
Maria Dusyk <fibrmirr@gmail.com>
2023-06-08 16:48:16 +0300

    Demoblaze

---
+++ cypress/support/pages/cartPage.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+class CartPage {
+    clickOnLink(linkText) {
+      cy.contains(linkText).click();
+    }
+  
+    verifyProduct(productName) {
+      cy.get('.col-lg-8').should('contain', productName);
+    }
+  }
+  
+  export default CartPage;
