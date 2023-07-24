0984b94f0737293238bb556ca5c2fa752396b2c8
neshtaa <you@example.com>
2023-07-24 19:51:38 +0300

    cypress8

---
+++ cypress/support/pages/cart.pageObject.js
@@ -0,0 +1,19 @@
+import PageObject from '../PageObject';
+
+class cartPageObject extends PageObject {
+  clickAddToTheCart() {
+    cy.contains(`a`, `Add to cart`)
+      .click();
+  }
+
+  assertProductInCart(product) {
+    cy.get('.success')
+      .should('contain.text', product);
+  }
+
+  clickPlaceOrder() {
+    cy.contains(`button`, `Place Order`).click();
+  }
+}
+
+export default cartPageObject;
