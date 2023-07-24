b2290eab5e6c58f3b06906d0facbb67781b9d67d
Dmytro Ieromin <dmytroyerominfcdk@gmail.com>
2023-07-24 19:50:16 +0300

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,48 @@
-/// <reference types='cypress' />
+import HomeAndCataloguePageObject
+  from '../support/pages/homeСatalogue.pageObject';
+import faker from 'faker';
+import CartPageObject from '../support/pages/cart.PageObject';
+import ProductPagePageObject
+  from '../support/pages/productPage.PageObject';
+const homePage = new HomeAndCataloguePageObject();
+const productPage = new ProductPagePageObject();
+const cartPage = new CartPageObject();
 
-describe('', () => {
+const testData = {
+  name: faker.name.firstName(),
+  country: faker.address.country(),
+  city: faker.address.city(),
+  creditCard: '1111222233334444',
+  month: faker.date.month(),
+  year: (() => {
+    const randomYear = faker.random.number({min: 2023,max: 2100});
+    return randomYear;
+  })(),
+  categoryName: 'Laptops',
+  productName: 'Sony vaio i7',
+  assertMessage: 'Thank you for your purchase!'
+};
+
+describe('Checkout process', () => {
   before(() => {
-
+    homePage.visit();
   });
 
-  it('', () => {
-
+  it('should allow to add items to the cart and make a purchase', () => {
+    productPage.visit();
+    productPage.clickOnAddToCartButton();
+    productPage.assertAllert();
+    homePage.clickOnLink('Cart');
+    cartPage.assertProduct(testData.productName);
+    cartPage.clickOnPlaceOrderButton();
+    cartPage.enterName(testData.name);
+    cartPage.enterCountry(testData.country);
+    cartPage.enterCity(testData.city);
+    cartPage.enterCreditCard(testData.creditCard);
+    cartPage.enterMonth(testData.month);
+    cartPage.enterYear(testData.year);
+    cartPage.clickOnPurchaseButton();
+    cartPage.assertAlert(testData.name,testData.creditCard);
+    cartPage.clickOkButton();
   });
 });
