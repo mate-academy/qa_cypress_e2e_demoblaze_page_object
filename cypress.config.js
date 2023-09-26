8e115c0ed9e832adba51cfae2965f0436f2818bf
Yelyzaveta <yelyzaveta.chmirova@gmail.com>
2023-09-26 18:36:24 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.demoblaze.com/',
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       on('task', {
         generateUser() {
