a0a23198be0866d7b5552bccb695fc831c91b0bc
Dmytro Ponomarov <ponomarovdmitry13@gmail.com>
2023-09-26 19:54:54 +0300

    Solution

--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -17,6 +17,91 @@ class HomeAndCataloguePageObject extends PageObject {
     cy.contains('.hrefch', product)
       .click();
   }
+
+  clickOnButton(button) {
+    cy.contains('.btn', button)
+      .click();
+  }
+
+  get userNameField() {
+    return cy.get('#name');
+  }
+
+  get userCountryField() {
+    return cy.get('#country');
+  }
+
+  get userCityField() {
+    return cy.get('#city');
+  }
+
+  get userCardField() {
+    return cy.get('#card');
+  }
+
+  get userMonthField() {
+    return cy.get('#month');
+  }
+
+  get userYearField() {
+    return cy.get('#year');
+  }
+
+  fillNameField(name) {
+    this.userNameField.type(name);
+  }
+
+  fillCountryField(country) {
+    this.userCountryField.type(country);
+  }
+
+  fillCityField(city) {
+    this.userCityField.type(city);
+  }
+
+  fillCardField(card) {
+    this.userCardField.type(card);
+  }
+
+  fillMonthField(month) {
+    this.userMonthField.type(month);
+  }
+
+  fillYearField(year) {
+    this.userYearField.type(year);
+  }
+
+  get purchaseText() {
+    return cy.get('.sweet-alert');
+  }
+
+  ModalWindowContainText(text) {
+    this.purchaseText.should('contain', text);
+  }
+
+  get randomName() {
+    return cy.get('.lead');
+  }
+
+  ModalWindowContainName(name) {
+    this.randomName.should('contain', name);
+  }
+
+  get randomCreditCard() {
+    return cy.get('.lead');
+  }
+
+  ModalWindowContainCard(card) {
+    this.randomName.should('contain', card);
+  }
+
+  get productName() {
+    return cy.get('.table');
+  }
+
+  cartContainProduct(product) {
+    this.productName.should('contain', product);
+  }
 }
 
 export default HomeAndCataloguePageObject;
