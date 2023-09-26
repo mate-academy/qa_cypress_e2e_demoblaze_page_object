537ed11e7f4453de9f38c985fda2a444d7a2dd2a
Serhii Tsybulko <serhii.tko@gmail.com>
2023-09-26 16:58:06 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.demoblaze.com/',
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       on('task', {
         generateUser() {
@@ -12,8 +15,18 @@ module.exports = defineConfig({
             email: 'test' + `${randomNumber}` + '@mail.com',
             password: 'Password12345!'
           };
+        },
+        generateOrderData() {
+          return {
+            name: faker.name.firstName(),
+            country: faker.address.country(),
+            city: faker.address.city(),
+            creditCard: faker.finance.creditCardNumber(),
+            month: Math.floor(Math.random() * (12 - 1 + 1)) + 1,
+            year: Math.floor(Math.random() * (2040 - 2023 + 1)) + 2023
+          };
         }
-      })
+      });
     }
   }
 });
