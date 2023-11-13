d9a33d60093e258811d269163b7bc76b8a95b8e7
Kristina Korolkova <kkorolkova94@gmail.com>
2023-11-13 18:27:50 +0200

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,41 @@
+import CartPage from '../support/pages/cart.pageObject';
+import HomeAndCataloguePageObject from '../support/pages/homeСatalogue.pageObject';
+import ContactFormPageObject from '../support/pages/contactForm.pageObject';
+import PageObject from '../support/PageObject';
+
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+const homeAndCatalogue = new HomeAndCataloguePageObject();
+const cartPage = new CartPage();
+const contactForm = new ContactFormPageObject();
+const page = new PageObject();
 
+describe('Order flow', () => {
+  before(() => {
+    page.visit('https://www.demoblaze.com/');
   });
 
-  it('', () => {
+  it('should provide an ability to add the product to a cart and make a checkout', () => {
+homeAndCatalogue.clickOnCategory('Laptops');
+    homeAndCatalogue.clickOnProduct('Sony vaio i7');
+    homeAndCatalogue.addToCart();
+    homeAndCatalogue.clickOnCart();
 
+    cartPage.assertProductInCart('Sony vaio i7');
+    cartPage.placeOrder();
+
+    contactForm.fillOrderForm(
+      'Kristina Korolkova',
+      'Ukraine',
+      'Kiliya',
+      '1234 5678 9110 3896',
+      '12', '2023'
+    );
+    contactForm.clickPurchaseButton();
+
+    contactForm.assertEnteredData(
+      'Thank you for your purchase!',
+      'Kristina Korolkova'
+    );
   });
 });
--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -1,21 +1,23 @@
 import PageObject from '../PageObject';
 
 class HomeAndCataloguePageObject extends PageObject {
-  url = '/index.html';
-
-  clickOnLink(linkName) {
-    cy.contains('.nav-link', linkName)
-      .click();
-  }
-
+visit() {
+    cy.visit('https://www.demoblaze.com/');
+}
   clickOnCategory(categoryName) {
     cy.contains('#itemc', categoryName)
       .click();
   }
 
-  clickOnProduct(product) {
-    cy.contains('.hrefch', product)
-      .click();
+  addToCart() {
+    cy.contains('Add to cart').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Product added');
+    });
+  }
+
+  clickOnCart() {
+    cy.contains('Cart').click();
   }
 }
 
