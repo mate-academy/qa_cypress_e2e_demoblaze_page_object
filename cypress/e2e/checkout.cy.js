2d0d711c05dada9cdc1b3c4b078a1e4a359d68d1
Zlata Yermilova <ermzlakl@gmail.com>
2023-11-13 16:42:30 +0200

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,33 @@
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+import HomeAndCataloguePageObject from '../support/pages/homeСatalogue.pageObject';
+import ContactFormPageObject from '../support/pages/contactForm.pageObject';
 
+const homeAndCatalogue = new HomeAndCataloguePageObject();
+const contactForm = new ContactFormPageObject();
+
+describe('Demoblaze checkout tests', () => {
+  before(() => {
+    cy.visit('/');
   });
 
-  it('', () => {
+  it('Should be ability to add the product to the cart and checkout', () => {
+    homeAndCatalogue.clickLaptopsBtn();
+    homeAndCatalogue.clickLaptopNameBtn();
+    homeAndCatalogue.clickAddToCartBtn();
+    homeAndCatalogue.clickWindowOkBtn();
+    homeAndCatalogue.clickCartBtn();
+    homeAndCatalogue.assertCartPageUrl();
 
+    contactForm.clickPlaceOrderBtn();
+    contactForm.nameField.type(username);
+    contactForm.countryField.type(country);
+    contactForm.cityField.type(city);
+    contactForm.cardField.type(card);
+    contactForm.monthField.type(month);
+    contactForm.yearField.type(year);
+    contactForm.clickPurchaseBtn();
+    contactForm.assertSuccessfulMessageModal();
+    contactForm.clickOKBtn();
   });
 });
