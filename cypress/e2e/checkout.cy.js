466baf335ddbd0c303d4e29f43c868d999e900c8
Dawid_Antkiewicz <dawidantkiew@gmail.com>
2023-12-14 16:15:32 +0100

    Add product to the cart

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,23 @@
 /// <reference types='cypress' />
 
+import HomeAndCataloguePageObject from "../support/pages/homeСatalogue.pageObject";
+import ProductDetailPageObject from "../support/pages/productDetailPage.pageObject"; 
+
+const homePage = new HomeAndCataloguePageObject;
+const productPage = new ProductDetailPageObject;
+
 describe('', () => {
   before(() => {
 
   });
 
   it('', () => {
-
+    homePage.visit();
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    productPage.addToCart;  
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Product added`);
+  })
   });
 });
