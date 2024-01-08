2d6e28f50c665aa03d1f1b2f8918ddbca77d6bc6
Rogers1275 <sashakondyuk@gmail.com>
2024-01-08 22:20:21 +0200

    added pom for adding product to the cart and purchasing it

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,49 @@
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+import PageObject from "../support/PageObject";
+import HomeAndCataloguePageObject from "../support/pages/homeСatalogue.pageObject";
 
+const homePage = new HomeAndCataloguePageObject();
+const pageObject = new PageObject();
+const faker = require("faker");
+
+const infoAboutUser = {
+  name: faker.name.firstName() + faker.name.lastName(),
+  country: faker.address.country(),
+  city: faker.address.city(),
+  creditCard: '9876123456789456',
+  month: faker.date.month(),
+  year: '1990'
+}
+
+describe('Demoblaze', () => {
+  before(() => {
+    homePage.visit();
   });
 
-  it('', () => {
+  it('should provide an ability to add item to the cart and buy it', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    homePage.buttonAddToCart();
 
+    pageObject.assertAllert('Product added');
+
+    homePage.clickOnLink('Cart');
+
+    homePage.assertProductInCart('Sony vaio i7');
+
+    homePage.buttonPlaceOrder();
+    homePage.getById('name', infoAboutUser.name);
+    homePage.getById('country', infoAboutUser.country);
+    homePage.getById('city', infoAboutUser.city);
+    homePage.getById('card', infoAboutUser.creditCard);
+    homePage.getById('month', infoAboutUser.month);
+    homePage.getById('year', infoAboutUser.year);
+    homePage.buttonPurchase();
+
+    homePage.assertDataAfterPurchase(infoAboutUser.name);
+    homePage.assertDataAfterPurchase(infoAboutUser.creditCard);
+
+    homePage.buttonOkAfterPurchase();
   });
 });
--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -17,6 +17,34 @@ class HomeAndCataloguePageObject extends PageObject {
     cy.contains('.hrefch', product)
       .click();
   }
+
+  buttonAddToCart() {
+    cy.get('.btn.btn-success.btn-lg').click();
+  }
+
+  buttonPlaceOrder() {
+    cy.get('.btn.btn-success').click();
+  }
+
+  buttonPurchase() {
+    cy.get('[onclick="purchaseOrder()"]').click()
+  }
+
+  buttonOkAfterPurchase() {
+    cy.get('.confirm').click()
+  }
+
+  assertProductInCart(product) {
+    cy.get('td').should('contain', product);
+  }
+
+  getById(id, fieldName) {
+    cy.get(`[id="${id}"]`).type(fieldName);
+  }
+
+  assertDataAfterPurchase(dataAboutUser) {
+    cy.get('.sweet-alert').should('contain', dataAboutUser)
+  }
 }
 
 export default HomeAndCataloguePageObject;
