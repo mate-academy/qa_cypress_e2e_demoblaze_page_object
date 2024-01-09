388a42afd712bc6af56d58be1063dd74c468cb04
Anna Mahda <148216090+AnnaMahda@users.noreply.github.com>
2024-01-09 13:55:04 +0200

    homeСatalogue.pageObject.js

--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -1,22 +1,37 @@
 import PageObject from '../PageObject';
-
 class HomeAndCataloguePageObject extends PageObject {
   url = '/index.html';
-
   clickOnLink(linkName) {
     cy.contains('.nav-link', linkName)
       .click();
   }
 
+  clickOnButton1(buttonName) {
+    cy.contains('.col-sm-12', buttonName)
+      .click();
+  }
+
+  clickOnButton2(buttonName) {
+    cy.contains('.btn-success', buttonName)
+      .click();
+  }
+
+  clickOnButton3(buttonName) {
+    cy.contains('.btn-primary', buttonName)
+      .click();
+  }
+
+   clickOnButton4(buttonName) {
+    cy.contains('.btn', buttonName)
+      .click();
+
   clickOnCategory(categoryName) {
     cy.contains('#itemc', categoryName)
       .click();
   }
-
   clickOnProduct(product) {
     cy.contains('.hrefch', product)
       .click();
   }
 }
-
 export default HomeAndCataloguePageObject;
