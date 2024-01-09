f9b4d0a7a8dc171fbc618535133a57d035ae6296
OlgaBelmega <143718672+OlgaBelmega@users.noreply.github.com>
2024-01-09 16:50:57 +0200

    Update checkout

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,46 @@
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+import { CheckoutPageObject } from "../support/pages/Demoblaze.pageObject";
+import ContactFormPageObject from "../support/pages/contactForm.pageObject";
 
-  });
 
-  it('', () => {
 
-  });
+describe('Demoblaze page', () => {
+  const checkoutPage = new CheckoutPageObject();
+  let user;
+  const checkcart = new ContactFormPageObject();
+
+beforeEach(() => {
+    cy.task('generateUser').then(generateUser => {
+   user = generateUser;
+    })
+  })
+ it('should visit the page and add the product to the cart', () => {
+    checkoutPage.visit();
+    checkoutPage.laptopslink.click();
+    checkoutPage.sonylink.click();
+    checkoutPage.addToCart.click();
+    checkoutPage.assertaddedmessage;
+});
+
+it.only('should be able to check the cart and purchase an order', () => {
+  checkoutPage.visit();
+  checkoutPage.laptopslink.click();
+  checkoutPage.sonylink.click();
+  checkoutPage.addToCart.click();
+  checkoutPage.assertaddedmessage;
+
+  checkcart.carturl.click();
+  checkcart.assertInTheCart;
+  checkcart.clickOnPlaceOrder.click();
+  checkcart.nameField.type(user.username);
+  checkcart.countryField.type('USA');
+  checkcart.cityField.type('New York');
+  checkcart.creditCartField.type('1478569874569874')
+  checkcart.monthField.type('3');
+  checkcart.yearField.type('2022');
+  checkcart.purchaseButton.click();
+  checkcart.confirmMessage;
+  checkcart.clickOn.click();
+});
 });
