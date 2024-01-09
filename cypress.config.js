c88f8a27f4fc357a6f07bf5e06208e2a4f3f18c2
Yuliia Siromakha <yuliya.o.siromakha@gmail.com>
2024-01-09 18:12:21 +0200

    config

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
+
 
 module.exports = defineConfig({
   e2e: {
@@ -9,8 +11,24 @@ module.exports = defineConfig({
           const randomNumber = Math.ceil(Math.random(1000) * 1000);
           return {
             username: faker.name.firstName() + `${randomNumber}`,
-            email: 'test' + `${randomNumber}` + '@mail.com',
-            password: 'Password12345!'
+            email: faker.internet.email(),
+            password: 'Password12345!',
+
+          };
+        }
+      })
+
+      on('task', {
+        createPlaceOrderUser() {
+    
+          return {
+            name: faker.name.firstName(),
+            country: faker.address.country(),
+            city: faker.address.city(),
+            credit_card: faker.finance.creditCardNumber(),
+            month: faker.date.month(),
+            year: faker.random.number({ min: 2020, max: new Date().getFullYear() })
+
           };
         }
       })
