aee3a0cd212ef41b2d35debb49e285e9acd89068
Dmytro_Yelisieiev <deliseev0482@gmail.com>
2024-01-09 15:06:14 +0200

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,42 @@
 /// <reference types='cypress' />
+import PageObject from '../support/PageObject';
+import HomeAndCataloguePageObject from '../support/pages/homeCatalogue.pageObject';
 
-describe('', () => {
+const faker = require('faker');
+
+const homePage = new HomeAndCataloguePageObject();
+const pageObject = new PageObject();
+
+const userInfo = {
+  name: faker.name.firstName(),
+  country: faker.address.country(),
+  city: faker.address.city(),
+  creditCard: faker.finance.creditCardNumber(),
+  month: faker.date.month(),
+  year: '2026'
+};
+describe('Home page', () => {
   before(() => {
-
+    homePage.visit();
   });
 
-  it('', () => {
-
+  it('should be able to add item to the cart and buy it', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    homePage.clickOnAddToCartButton();
+    pageObject.assertAllert('Product added');
+    homePage.clickOnLink('Cart');
+    homePage.assertItemInCart('Sony vaio i7');
+    homePage.clickOnButtonPlaceOrder();
+    homePage.findFieldById('name', userInfo.name);
+    homePage.findFieldById('country', userInfo.country);
+    homePage.findFieldById('city', userInfo.city);
+    homePage.findFieldById('card', userInfo.creditCard);
+    homePage.findFieldById('month', userInfo.month);
+    homePage.findFieldById('year', userInfo.year);
+    homePage.clickOnPurchaseButton();
+    homePage.assertDataAfterPurchase(userInfo.name);
+    homePage.assertDataAfterPurchase(userInfo.creditCard);
+    homePage.clickOnOkAfterPurchase();
   });
 });
