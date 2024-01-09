3bda303088ca2f11237aa262639b7621fbaa0022
Yura Kisilchuk <yurkis02@gmail.com>
2024-01-09 10:18:42 +0200

    automated Purchase Flow for Sony Vaio i7 in Demoblaze App using POM

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,43 @@
 /// <reference types='cypress' />
+const faker = require('faker');
 
-describe('', () => {
+import PageObject from "../support/PageObject";
+import HomeAndCataloguePageObject from "../support/pages/homeСatalogue.pageObject";
+
+const homePage = new HomeAndCataloguePageObject();
+const pageObject = new PageObject();
+
+let userInfo = {
+  name: faker.name.firstName(),
+  country: faker.address.country(),
+  city: faker.address.city(),
+  creditCard: '1111222233334444',
+  month: faker.date.month(),
+  year: '2002'
+}
+
+describe('Home page', () => {
   before(() => {
-
+    homePage.visit();
   });
 
-  it('', () => {
-
+  it('should provide an ability to add item to the cart and buy it', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    homePage.clickOnAddToCartButton();
+    pageObject.assertAllert('Product added');
+    homePage.clickOnLink('Cart');
+    homePage.assertItemInCart('Sony vaio i7');
+    homePage.clickOnButtonPlaceOrder();
+    homePage.findById('name', userInfo.name);
+    homePage.findById('country', userInfo.country);
+    homePage.findById('city', userInfo.city);
+    homePage.findById('card', userInfo.creditCard);
+    homePage.findById('month', userInfo.month);
+    homePage.findById('year', userInfo.year);
+    homePage.clickOnPurchase();
+    homePage.assertDataAfterPurchase(userInfo.name);
+    homePage.assertDataAfterPurchase(userInfo.creditCard);
+    homePage.clickOnOkAfterPurchase();
   });
 });
--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -17,6 +17,37 @@ class HomeAndCataloguePageObject extends PageObject {
     cy.contains('.hrefch', product)
       .click();
   }
-}
 
+  clickOnAddToCartButton() {
+    cy.get('[onclick="addToCart(9)"]')
+      .click();
+  }
+
+  assertItemInCart(product) {
+    cy.get('#tbodyid').should('contain', product);
+  }
+
+  clickOnButtonPlaceOrder() {
+    cy.get('.btn.btn-success')
+      .click();
+  }
+
+  findFieldById(id, fieldName) { 
+    cy.get(`[id=${id}]`)
+      .type(fieldName);
+  }
+
+  clickOnPurchase() {
+    cy.get('[onclick="purchaseOrder()"]')
+      .click();
+  }
+
+  assertDataAfterPurchase(data) {
+    cy.get('.sweet-alert').should('contain', data);
+  }
+
+  clickOnOkAfterPurchase() {
+    cy.get('.confirm.btn.btn-lg.btn-primary').click();
+  }
+}
 export default HomeAndCataloguePageObject;
