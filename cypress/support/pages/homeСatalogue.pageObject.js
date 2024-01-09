978ba72833365c7732c43d7166cb4d2b47c3a85d
Yuliia Siromakha <yuliya.o.siromakha@gmail.com>
2024-01-09 18:23:45 +0200

    order form pom

--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -3,20 +3,32 @@ import PageObject from '../PageObject';
 class HomeAndCataloguePageObject extends PageObject {
   url = '/index.html';
 
-  clickOnLink(linkName) {
-    cy.contains('.nav-link', linkName)
+  clickOnLink(Laptops) {
+    cy.contains('[onclick^="byCat"]', 'Laptops')
       .click();
   }
 
-  clickOnCategory(categoryName) {
-    cy.contains('#itemc', categoryName)
+  clickOnCategory(Sony) {
+    cy.contains('a', 'Sony vaio i7')
       .click();
   }
 
-  clickOnProduct(product) {
-    cy.contains('.hrefch', product)
+  clickOnProduct(Product) {
+    cy.contains('[onclick="addToCart(9)"]', 'Add to cart')
       .click();
   }
+
+
+  clickOnCart(Cart) {
+    cy.contains('[href="cart.html"]', 'Cart')
+      .click();
+  }
+
+  checkContainProduct(Check) {
+    cy.get('td').should('contain.text', 'Sony vaio i7');
+      
+  }
+
 }
 
 export default HomeAndCataloguePageObject;
---
+++ cypress/support/pages/placeOrderForm.pageObject.js
@@ -0,0 +1,63 @@
+
+import PageObject from "../PageObject";
+
+class PlaceOrderForm extends PageObject {
+    url = '/cart.html';
+
+
+    clickOnPlaceOrder(Order) {
+        cy.contains('[data-target="#orderModal"]', 'Place Order').click();
+          
+      }
+
+      get fieldName(){
+        return cy.get('#name');
+      }
+
+      get fieldCountry(){
+        return cy.get('#country');
+      }
+
+      get fieldCity(){
+        return cy.get('#city');
+      }
+
+      get fieldCard(){
+        return cy.get('#card');
+      }
+
+      get fieldMonth(){
+        return cy.get('#month');
+      }
+
+      get fieldYear(){
+        return cy.get('#year');
+      }
+
+       clickOnPurchase(){
+         cy.contains('.btn-primary', 'Purchase').click({force:true});
+      }
+
+      popUpIsVisible(){
+        cy.get('.sa-placeholder').should('be.visible');
+      }
+
+      popUpContainText(){
+        cy.contains('h2', 'Thank you for your purchase!').should('contain.text', 'Thank you for your purchase!');
+        
+      }
+
+      get nameIsDisplayedOnPopUp(){
+        return cy.contains('p', 'Name:')
+      }
+
+      get numberOfCardIsDisplayedOnPopUp(){
+        return cy.contains('p', 'Card Number:')
+      }
+
+      get clickOnOk(){
+        return cy.get('[style="display: inline-block;"]')
+      }
+};
+
+export default PlaceOrderForm;
