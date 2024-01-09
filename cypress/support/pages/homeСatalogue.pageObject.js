73eda0ad75711dee4b7b077fbda05431d26333fa
Rogers1275 <sashakondyuk@gmail.com>
2024-01-09 14:42:17 +0200

    deleted getters from homeCatalogue

--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -17,58 +17,6 @@ class HomeAndCataloguePageObject extends PageObject {
     cy.contains('.hrefch', product)
       .click();
   }
-
-  get addToCartBtn() {
-    return cy.get('.btn.btn-success.btn-lg')
-  }
-
-  clickOnAddToCartBtn() {
-    this.addToCartBtn.click();
-  }
-
-  get placeOrderBtn() {
-    return cy.get('.btn.btn-success')
-  }
-
-  clickOnPlaceOrderBtn() {
-    this.placeOrderBtn.click();
-  }
-
-  get purchaseBtn() {
-    return cy.get('[onclick="purchaseOrder()"]')
-  }
-
-  clickOnPurchaseBtn() {
-    this.purchaseBtn.click()
-  }
-
-  get okAfterPurchaseBtn() {
-    return cy.get('.confirm')
-  }
-
-  clickOnOkAfterPurchaseBtn() {
-    this.okAfterPurchaseBtn.click()
-  }
-  
-  get productInCart() {
-    return cy.get('td')
-  }
-
-  assertProductInCart(product) {
-    this.productInCart.should('contain', product);
-  }
-
-  getById(id, fieldName) {
-    cy.get(`[id="${id}"]`).type(fieldName);
-  }
-
-  get dataAfterPurchase() {
-    return cy.get('.sweet-alert')
-  }
-
-  assertDataAfterPurchase(dataAboutUser) {
-    this.dataAfterPurchase.should('contain', dataAboutUser)
-  }
 }
 
 export default HomeAndCataloguePageObject;
