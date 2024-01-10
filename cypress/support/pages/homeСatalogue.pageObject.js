f24f86527b3f853c419665c06ffa59d32250b436
Nikolaichuk Artem <tompsoq@gmail.com>
2024-01-10 11:32:28 +0200

    solution for Cypress: Demoblaze

--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -17,6 +17,58 @@ class HomeAndCataloguePageObject extends PageObject {
     cy.contains('.hrefch', product)
       .click();
   }
-}
 
+  get addToCart() {
+    return cy.get('[onclick="addToCart(9)"]');
+  }
+
+  clickOnAddToCartButton() {
+    this.addToCart.click();
+  }
+
+  get itemId() {
+    return cy.get('#tbodyid');
+  }
+
+  assertItemInCart(product) {
+    this.itemId.should('contain', product);
+  }
+
+  get buttonPlaceOrder() {
+    return cy.get('.btn.btn-success');
+  }
+
+  clickOnButtonPlaceOrder() {
+    this.buttonPlaceOrder.click();
+  }
+
+  findFieldById(id, fieldName) {
+    cy.get(`[id=${id}]`)
+      .type(fieldName);
+  }
+
+  get purchaseButton() {
+    return cy.get('[onclick="purchaseOrder()"]');
+  }
+
+  clickOnPurchaseButton() {
+    this.purchaseButton.click();
+  }
+
+  get allertAfterPurchase() {
+    return cy.get('.sweet-alert');
+  }
+
+  assertDataAfterPurchase(data) {
+    this.allertAfterPurchase.should('contain', data);
+  }
+
+  get okButton() {
+    return cy.get('.confirm.btn.btn-lg.btn-primary');
+  }
+
+  clickOnOkAfterPurchase() {
+    this.okButton.click();
+  }
+}
 export default HomeAndCataloguePageObject;
