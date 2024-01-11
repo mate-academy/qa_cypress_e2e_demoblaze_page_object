da9b97d598b4edfd36edff6f2d73626cbbe80a63
Yuliia Siromakha <146849831+yuliia-sirom@users.noreply.github.com>
2024-01-11 19:37:07 +0200

    Update homeCatalogue.pageObject.js

--- cypress/support/pages/homeCatalogue.pageObject.js
+++ cypress/support/pages/homeCatalogue.pageObject.js
@@ -3,31 +3,31 @@ import PageObject from '../PageObject';
 class HomeAndCataloguePageObject extends PageObject {
   url = '/index.html';
 
-  get clickOnLink() {
-    return cy.contains('[onclick="byCat('notebook')"]', 'Laptops');
-      
+
+
+  get clickOnLink(){
+    return cy.contains('[onclick^="byCat"]', 'Laptops');
+
   }
 
-  get clickOnCategory() {
+  get clickOnCategory(){
     return cy.contains('a', 'Sony vaio i7');
-      
   }
 
-  get clickOnProduct() {
-    return cy.contains('[onclick="addToCart(9)"]', 'Add to cart');
-      
+  get clickOnProduct(){
+    return cy.contains('[onclick="addToCart(9)"]', 'Add to cart');     
   }
 
 
-  get clickOnCart() {
+  get clickOnCart(){
     return cy.contains('[href="cart.html"]', 'Cart');
+  }
+
+  get checkContainProduct(){
+    return cy.get('td');
       
   }
 
-  get checkContainProduct() {
-    return cy.get('td').should('contain.text', 'Sony vaio i7');
-      
-  }
 }
 
 export default HomeAndCataloguePageObject;
