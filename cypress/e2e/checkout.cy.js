8e2dbc7903d20d1d4a8b3b80c1413db56da9f956
Yuliia Siromakha <146849831+yuliia-sirom@users.noreply.github.com>
2024-01-13 19:42:53 +0200

    Second Update checkout.cy.js

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -11,7 +11,7 @@ const pagObj = new PageObject();
 const orderPage = new PlaceOrderForm();
 
 
-describe('', () => {
+describe('Product', () => {
 
   beforeEach(() => { 
     cy.task('createPlaceOrderUser').then(createPlaceOrderUser => {
@@ -23,14 +23,14 @@ describe('', () => {
   it('should provide an ability to add product to the cart', () => {
 
     homePage.visit();    
-    homePage.clickOnLink.click();
-    homePage.clickOnCategory.click();
-    homePage.clickOnProduct.click();
+    homePage.clickOnLink('Laptops');
+    homePage.clickOnCategory('Sony vaio i7');
+    homePage.clickOnProduct('Add to cart');
     cy.wait(2000);
-    pagObj.assertAllert('Product added');
+    homePage.assertAllert('Product added');
 
-    homePage.clickOnCart.click();
-    homePage.checkContainProduct.should('contain.text', 'Sony vaio i7');
+    homePage.clickOnCart('cart');
+    homePage.checkContainProduct('Sony vaio i7');
 
   });
 
@@ -38,31 +38,29 @@ describe('', () => {
   it('should provide an ability to place an order', () => {
 
     homePage.visit();    
-    homePage.clickOnLink.click();
-    homePage.clickOnCategory.click();
-    homePage.clickOnProduct.click();
-    cy.wait(2000);
-    pagObj.assertAllert('Product added');
-
-    homePage.clickOnCart.click();
+    homePage.clickOnLink('Laptops');
+    homePage.clickOnCategory('Sony vaio i7');
+    homePage.clickOnProduct('Add to cart');
+    homePage.clickOnCart('cart');
 
       orderPage.visit();
-      orderPage.clickOnPlaceOrder.click()
-      orderPage.fieldName.type(user.name);
-      orderPage.fieldCountry.type(user.country);
-      orderPage.fieldCity.type(user.city);
-      orderPage.fieldCard.type(user.credit_card);
-      orderPage.fieldMonth.type(user.month);
-      orderPage.fieldYear.type(user.year);
+      orderPage.clickOnPlaceOrder('Place Order');
+      orderPage.typeFieldName(user.name);
+      orderPage.typeFieldCountry(user.country);
+      orderPage.typeFieldCity(user.city);
+      orderPage.typeFieldCard(user.credit_card);
+      orderPage.typeFieldMonth(user.month);
+      orderPage.typeFieldYear(user.year);
 
       orderPage.clickOnPurchase();
 
-      orderPage.popUpIsVisible.should('be.visible');
-      orderPage.popUpContainText.should('contain.text', 'Thank you for your purchase!');
+      orderPage.popUpIsVisible;
+      orderPage.popUpContainText('Thank you for your purchase!');
 
-      orderPage.nameIsDisplayedOnPopUp.should('contain', user.credit_card);
+      orderPage.nameIsDisplayedOnPopUp(user.name);
+      orderPage.numberOfCardIsDisplayedOnPopUp(user.credit_card);
 
-      orderPage.clickOnOk.click({force: true});
+      orderPage.clickOnOk
 
 
 
