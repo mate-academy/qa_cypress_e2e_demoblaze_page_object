ad949a328c76ac93f0a220802c69fe3c6717b7e6
OlgaBelmega <143718672+OlgaBelmega@users.noreply.github.com>
2024-01-15 21:22:59 +0200

    Update

--- cypress/support/pages/contactForm.pageObject.js
+++ cypress/support/pages/contactForm.pageObject.js
@@ -1,28 +1,87 @@
 import PageObject from '../PageObject';
 
 export class ContactFormPageObject extends PageObject {
-  url = '/index.html';
+ url = '/cart.html';
 
-  url = '/index.html';
-
-  clickOnLink(linkName) {
-    cy.contains('.nav-link', linkName)
-      .click();
+  get productTable() {
+    return cy.get('[class="table-responsive"]');
   }
 
-  clickOnCategory(categoryName) {
-    cy.contains('#itemc', categoryName)
-      .click();
+  get sweetAlert() {
+    return cy.get('[class="sweet-alert  showSweetAlert visible"]');
   }
 
-  clickOnProduct(product) {
-    cy.contains('.hrefch', product)
-      .click();
+  get nameField() {
+    return cy.get('#name');
+  }
+
+  get countryField() {
+    return cy.get('#country');
+  }
+
+  get cityField() {
+    return cy.get('#city');
+  }
+
+  get creditCardField() {
+    return cy.get('#card');
+  }
+
+  get monthField() {
+    return cy.get('#month');
+  }
+
+  get yearField() {
+    return cy.get('#year');
+  }
+
+  assertProductInCart(product) {
+    this.productTable
+      .should('contain', product);
   }
 
   clickOnButton(button) {
     cy.contains('.btn', button)
       .click();
+  }
+
+  typeName(name) {
+    this.nameField.type(name);
+  }
+
+  typeCountry(country) {
+    this.countryField.type(country);
+  }
+
+  typeCity(city) {
+    this.cityField.type(city);
+  }
+
+  typeCreditCard(creditCard) {
+    this.creditCardField.type(creditCard);
+  }
+
+  typeMonth(month) {
+    this.monthField.type(month);
+  }
+
+  typeYear(year) {
+    this.yearField.type(year);
+  }
+
+  assertName(name) {
+    this.sweetAlert
+      .should('contain', name);
+  }
+
+  assertCreditCard(creditCard) {
+    this.sweetAlert
+      .should('contain', creditCard);
+  }
+
+  assertSuccessMessage(successMessage) {
+    this.sweetAlert
+      .should('contain', successMessage);
   }
 }
 
