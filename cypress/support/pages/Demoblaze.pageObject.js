1ec8a49351b9e628b84d26a1d0b42198c6905962
OlgaBelmega <143718672+OlgaBelmega@users.noreply.github.com>
2024-01-16 09:06:53 +0200

    Update Demoblaze

--- cypress/support/pages/Demoblaze.pageObject.js
+++ cypress/support/pages/Demoblaze.pageObject.js
@@ -3,26 +3,86 @@ import faker from 'faker';
 import ContactFormPageObject from "./contactForm.pageObject";
 
 export class CheckoutPageObject extends PageObject {
- url = '/index.html';
+  url = '/cart.html';
 
-  clickOnLink(linkName) {
-    cy.contains('.nav-link', linkName)
-      .click();
+  get nameField() {
+    return cy.get('#name');
   }
 
-  clickOnCategory(categoryName) {
-    cy.contains('#itemc', categoryName)
-      .click();
+  get countryField() {
+    return cy.get('#country');
   }
 
-  clickOnProduct(product) {
-    cy.contains('.hrefch', product)
-      .click();
+  get cityField() {
+    return cy.get('#city');
   }
 
-  clickOnButton(button) {
-    cy.contains('.btn', button)
-      .click();
+  get creditCardField() {
+    return cy.get('#card');
+  }
+
+  get monthField() {
+    return cy.get('#month');
+  }
+
+  get yearField() {
+    return cy.get('#year');
+  }
+
+  get makePurchaseBtn() {
+    return cy.contains('.btn', 'Purchase');
+  }
+
+  get addToCartBtn() {
+    return cy.contains('.btn', 'Add to cart');
+  }
+
+  get placeOrderBtn() {
+    return cy.contains('.btn', 'Place Order');
+  }
+
+  itemInCart(item) {
+    cy.get('td').should('contain', item);
+  }
+
+  purchaseDone(alertMessage) {
+    cy.get('.sweet-alert').should('contain', alertMessage);
+  }
+
+  typeName(name) {
+    this.nameField.type(name, { force: true });
+  }
+
+  typeCountry(country) {
+    this.countryField.type(country, { force: true });
+  }
+
+  typeCity(city) {
+    this.cityField.type(city, { force: true });
+  }
+
+  typeCard(creditCard) {
+    this.creditCardField.type(creditCard, { force: true });
+  }
+
+  typeMonth(month) {
+    this.monthField.type(month, { force: true });
+  }
+
+  typeYear(year) {
+    this.yearField.type(year, { force: true });
+  }
+
+  clickOnaddToCartBtn() {
+    this.addToCartBtn.click();
+  }
+
+  clickOnmakePurchaseBtn() {
+    this.makePurchaseBtn.click();
+  }
+
+  clickOnplaceOrderBtn() {
+    this.placeOrderBtn.click();
   }
 }
 
