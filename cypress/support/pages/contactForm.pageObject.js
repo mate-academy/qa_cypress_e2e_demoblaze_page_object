bef7ba624c76dedc8be0e81e5ff640261caefbd8
OlgaBelmega <143718672+OlgaBelmega@users.noreply.github.com>
2024-01-16 09:13:16 +0200

    Update contactForm

--- cypress/support/pages/contactForm.pageObject.js
+++ cypress/support/pages/contactForm.pageObject.js
@@ -1,87 +1,21 @@
 import PageObject from '../PageObject';
 
-export class ContactFormPageObject extends PageObject {
- url = '/cart.html';
+class ContactFormPageObject extends PageObject {
+  url = '/index.html';
 
-  get productTable() {
-    return cy.get('[class="table-responsive"]');
-  }
-
-  get sweetAlert() {
-    return cy.get('[class="sweet-alert  showSweetAlert visible"]');
-  }
-
-  get nameField() {
-    return cy.get('#name');
-  }
-
-  get countryField() {
-    return cy.get('#country');
-  }
-
-  get cityField() {
-    return cy.get('#city');
-  }
-
-  get creditCardField() {
-    return cy.get('#card');
-  }
-
-  get monthField() {
-    return cy.get('#month');
-  }
-
-  get yearField() {
-    return cy.get('#year');
-  }
-
-  assertProductInCart(product) {
-    this.productTable
-      .should('contain', product);
-  }
-
-  clickOnButton(button) {
-    cy.contains('.btn', button)
+  clickOnLink(linkName) {
+    cy.contains('.nav-link', linkName)
       .click();
   }
 
-  typeName(name) {
-    this.nameField.type(name);
+  clickOnCategory(categoryName) {
+    cy.contains('#itemc', categoryName)
+      .click();
   }
 
-  typeCountry(country) {
-    this.countryField.type(country);
-  }
-
-  typeCity(city) {
-    this.cityField.type(city);
-  }
-
-  typeCreditCard(creditCard) {
-    this.creditCardField.type(creditCard);
-  }
-
-  typeMonth(month) {
-    this.monthField.type(month);
-  }
-
-  typeYear(year) {
-    this.yearField.type(year);
-  }
-
-  assertName(name) {
-    this.sweetAlert
-      .should('contain', name);
-  }
-
-  assertCreditCard(creditCard) {
-    this.sweetAlert
-      .should('contain', creditCard);
-  }
-
-  assertSuccessMessage(successMessage) {
-    this.sweetAlert
-      .should('contain', successMessage);
+  clickOnProduct(product) {
+    cy.contains('.hrefch', product)
+      .click();
   }
 }
 
