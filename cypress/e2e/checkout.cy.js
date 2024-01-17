af706aa5bff42eb74388be7b27763992338e41d5
Dmytro Horodchuk <dmytro.horodchuk@gmail.com>
2024-01-17 11:43:07 +0200

    Tests added

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,46 @@
 /// <reference types='cypress' />
+import faker from 'faker';
+import HomeAndCataloguePageObject
+  from '../support/pages/homeСatalogue.pageObject';
 
-describe('', () => {
-  before(() => {
+const homePage = new HomeAndCataloguePageObject();
+const testData = {
+  name: faker.name.firstName(),
+  country: faker.random.words(),
+  city: faker.random.words(),
+  card: faker.finance.creditCardNumber(),
+  year: 2023,
+  month: 12
+};
 
+describe('Product store', () => {
+  beforeEach(() => {
+    cy.visit('/');
   });
 
-  it('', () => {
+  // eslint-disable-next-line max-len
+  it('should have an ability to add product to the card and make checkout', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    homePage.addToCart('Add to cart');
 
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal(`Product added`);
+    });
+    homePage.cartMenu('Cart');
+    cy.get('.success > :nth-child(2)').should('contain', 'Sony vaio i7');
+    homePage.placeOrder('Place Order');
+    homePage.typeName(testData.name);
+    homePage.typeCountry(testData.country);
+    homePage.typeCity(testData.city);
+    homePage.typeCard(testData.card);
+    homePage.typeYear(testData.year);
+    homePage.typeMonth(testData.month);
+    homePage.click('Purchase');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000);
+    cy.get('.sweet-alert > h2')
+      .should('contain', 'Thank you for your purchase!');
+    homePage.clickOk('OK');
   });
 });
--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -17,6 +17,61 @@ class HomeAndCataloguePageObject extends PageObject {
     cy.contains('.hrefch', product)
       .click();
   }
-}
 
-export default HomeAndCataloguePageObject;
+  addToCart(button) {
+    cy.contains('.col-sm-12 > .btn', button)
+      .click();
+  }
+
+  cartMenu(menuItem) {
+    cy.contains('#cartur', menuItem)
+      .click();
+  }
+
+  placeOrder(Item) {
+    cy.contains('.col-lg-1 > .btn', Item)
+      .click();
+  }
+
+  click(zItem) {
+    // eslint-disable-next-line max-len
+    cy.contains('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary', zItem)
+      .click();
+  }
+
+  clickOk(yItem) {
+    // eslint-disable-next-line max-len
+    cy.contains('.confirm', yItem)
+      .click();
+  }
+
+  typeName(argument) {
+    cy.get('#name')
+      .type(argument);
+  }
+
+  typeCountry(argument1) {
+    cy.get('#country')
+      .type(argument1);
+  }
+
+  typeCity(argument2) {
+    cy.get('#city')
+      .type(argument2);
+  }
+
+  typeCard(argument3) {
+    cy.get('#card')
+      .type(argument3);
+  }
+
+  typeMonth(argument4) {
+    cy.get('#month')
+      .type(argument4);
+  }
+
+  typeYear(argument5) {
+    cy.get('#year')
+      .type(argument5);
+  }
+}
