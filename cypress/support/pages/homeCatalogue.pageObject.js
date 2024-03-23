a5df948fb1867a5e4af78088521fb5c0732add9f
AnatoliiTsymbaliuk <anatoliitsimbaliuk13@gmail.com>
2024-03-23 15:14:24 +0200

    Solution

---
+++ cypress/support/pages/homeCatalogue.pageObject.js
@@ -0,0 +1,28 @@
+import PageObject from '../PageObject';
+
+class HomeAndCataloguePageObject extends PageObject {
+  url = '/index.html';
+
+  clickOnLink(linkName) {
+    cy.contains('.nav-link', linkName)
+      .click();
+  }
+
+  clickOnCategory(categoryName) {
+    cy.contains('#itemc', categoryName)
+      .click();
+  }
+
+  clickOnProduct(product) {
+    cy.contains('.hrefch', product)
+      .click();
+  }
+
+  clickOnAddToCartButton() {
+    cy.contains('.btn-success', 'Add to cart')
+    .click();
+  }
+
+}
+
+export default HomeAndCataloguePageObject;
