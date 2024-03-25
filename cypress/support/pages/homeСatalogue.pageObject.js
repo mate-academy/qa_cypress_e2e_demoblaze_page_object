16b18a2939f45b4ab9ae2275a02251be43625672
Пользователь <wetball74@gmail.com>
2024-03-25 02:00:05 +0200

    First try

--- cypress/support/pages/homeСatalogue.pageObject.js
+++ cypress/support/pages/homeСatalogue.pageObject.js
@@ -3,6 +3,14 @@ import PageObject from '../PageObject';
 class HomeAndCataloguePageObject extends PageObject {
   url = '/index.html';
 
+  get addToChartButton() {
+    return cy.get('[onclick="addToCart(9)"]');
+  }
+
+  get placeOrderButton() {
+    return cy.contains('.btn', 'Place Order');
+  }
+
   clickOnLink(linkName) {
     cy.contains('.nav-link', linkName)
       .click();
@@ -17,6 +25,15 @@ class HomeAndCataloguePageObject extends PageObject {
     cy.contains('.hrefch', product)
       .click();
   }
-}
 
+  assertAlertMessage(alertMessage) {
+    return cy.on('window:alert', (alert) => {
+      expect(alert).to.include(alertMessage);
+    });
+  }
+
+  assertProductInCart(productName) {
+    return cy.get('#tbodyid').should('contain', productName);
+  }
+}
 export default HomeAndCataloguePageObject;
