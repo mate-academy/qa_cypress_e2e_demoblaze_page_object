f1d1c0bee8348d01e5df85c6c6579af432032edf
Oleksii <aleksey.gudilkin@gmail.com>
2024-05-20 18:18:36 +0300

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,50 @@
+/* eslint-disable max-len */
 /// <reference types='cypress' />
+import HomeAndCataloguePageObject from '../support/pages/homeСatalogue.pageObject';
+import PageObject from '../support/PageObject';
+import CartPageObject from '../support/pages/cart.pageObject';
+import { faker } from '@faker-js/faker';
 
-describe('', () => {
+describe('checkout', () => {
+  const homePage = new HomeAndCataloguePageObject();
+  const pageObject = new PageObject();
+  const cartPage = new CartPageObject();
+
+  const testData = {
+    name: faker.person.firstName(),
+    country: faker.location.country(),
+    city: faker.location.city(),
+    creditCard: faker.finance.creditCardNumber(),
+    month: Math.floor(Math.random() * 13),
+    year: '200' + Math.floor(Math.random() * 9),
+    successMessage: 'Product added'
+  };
+
   before(() => {
-
+    homePage.visit();
   });
 
-  it('', () => {
+  it('is able to sell a product', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    homePage.addToCartBtn.click();
 
+    pageObject.assertAllert(testData.successMessage);
+
+    homePage.clickOnLink('Cart');
+    cartPage.findProductInCart('Sony vaio i7');
+    cartPage.Btn('Place Order');
+    cartPage.NameInput.type(testData.name);
+    cartPage.CountryInput.type(testData.country);
+    cartPage.CityInput.type(testData.city);
+    cartPage.CreditCardInput.type(testData.creditCard);
+    cartPage.MonthInput.type(testData.month);
+    cartPage.YearInput.type(testData.year);
+
+    cartPage.Btn('Purchase');
+
+    cartPage.OrderInformation(testData.creditCard);
+    cartPage.OrderInformation(testData.name);
+    cartPage.OkBtn.click();
   });
 });
--- cypress/support/PageObject.js
+++ cypress/support/PageObject.js
@@ -4,6 +4,8 @@ class PageObject {
   }
 
   assertAllert(alertMessage) {
+  // if we don't use cy.wait here, the assert is skipped
+    cy.wait(1000);
     cy.on('window:alert', (alert) => {
       expect(alert).to.eq(alertMessage);
     });
