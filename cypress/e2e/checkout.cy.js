545fa51bd24a60655343bbd1c44e08eab4a818b7
Serhii Vasyliev <vv44980@gmail.com>
2024-07-09 08:19:39 -0600

    tests are done

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,21 @@
 /// <reference types='cypress' />
+import { LaptopsTab } from '../support/pages/customerUserWay';
 
-describe('', () => {
-  before(() => {
-
+const Laptops = new LaptopsTab();
+describe('Userflow using POM', () => {
+  before('open webpage', () => {
+    Laptops.navigateToHome();
   });
 
-  it('', () => {
-
+  it('full userway', () => {
+    Laptops.clickLaptopTab();
+    Laptops.clickSonyVaio7();
+    Laptops.addToTheCart();
+    Laptops.clickCartLink();
+    Laptops.checkItemInTheCart();
+    Laptops.clickThePlaceOrderButton();
+    Laptops.filoutForm();
+    Laptops.clickPurchase();
+    Laptops.clickOkButton();
   });
 });
