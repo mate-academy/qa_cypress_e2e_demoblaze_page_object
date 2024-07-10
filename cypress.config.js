4031e5399d07f6417318e4b3517b1b7a1a43d5a8
Serhii Vasyliev <vv44980@gmail.com>
2024-07-09 22:54:32 -0600

    tests

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     baseUrl: 'https://www.demoblaze.com/',
     setupNodeEvents(on, config) {
       on('task', {
-        generateData() {
+        generateDataForUser() {
           const randomNumber = Math.ceil(Math.random(1000) * 1000);
           return {
             username: faker.name.firstName() + `${randomNumber}`,
--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,21 +1,21 @@
 /// <reference types='cypress' />
-import { LaptopsTab } from '../support/pages/customerUserWay';
+import { SonyVaio7 } from '../support/pages/customerUserWay';
 
-const Laptops = new LaptopsTab();
-describe('Userflow using POM', () => {
-  before('open webpage', () => {
-    Laptops.navigateToHome();
+const sonyVaio7 = new SonyVaio7();
+describe('Customer Laptop Purchase Flow', () => {
+  before('Navigate to Home Page', () => {
+    sonyVaio7.navigateToHome();
   });
 
-  it('full userway', () => {
-    Laptops.clickLaptopTab();
-    Laptops.clickSonyVaio7();
-    Laptops.addToTheCart();
-    Laptops.clickCartLink();
-    Laptops.checkItemInTheCart();
-    Laptops.clickThePlaceOrderButton();
-    Laptops.filoutForm();
-    Laptops.clickPurchase();
-    Laptops.clickOkButton();
+  it('should complete the purchase of a laptop', () => {
+    sonyVaio7.clickLaptopTab();
+    sonyVaio7.clickSonyVaio7();
+    sonyVaio7.addToTheCart();
+    sonyVaio7.clickCartLink();
+    sonyVaio7.checkItemInTheCart();
+    sonyVaio7.clickThePlaceOrderButton();
+    sonyVaio7.fillOutTheBuyingForm();
+    sonyVaio7.clickPurchase();
+    sonyVaio7.clickOkButton();
   });
 });
