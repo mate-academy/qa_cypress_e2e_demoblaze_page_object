07f86dbc4c78e437c91d811c900b9498d8f665fa
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-11 15:01:07 +0300

    solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,30 @@
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+import HomeAndCartPageObject from '../support/pages/homeCatalogCart.pageObject';
+import HomeAndCataloguePageObject
+  from '../support/pages/homeСatalogue.pageObject';
 
+const homeCatalogCartPage = new HomeAndCartPageObject();
+const nav = new HomeAndCataloguePageObject();
+
+describe('', () => {
+  const product = 'Sony vaio i7';
+
+  before(() => {
+    homeCatalogCartPage.visit();
+    cy.task('generateUser').then((user) => user).as('user');
   });
 
-  it('', () => {
+  it('should buy Sony vaio i7 laptop', function() {
+    homeCatalogCartPage.clickOnTheCategory('Laptops');
+    homeCatalogCartPage.clickOnDeterminedLink(product);
+    homeCatalogCartPage.clickOnDeterminedLink('Add to cart');
+    homeCatalogCartPage.checkAlertWindow();
+    nav.clickOnLink('Cart');
+    homeCatalogCartPage.assertCart(product);
 
+    cy.get('button[data-target="#orderModal"]').click();
+
+    homeCatalogCartPage.sendTheOrder(this.user.username);
   });
 });
---
+++ cypress/support/constants/userInfo.js
@@ -0,0 +1,7 @@
+export const orderData = {
+  country: 'Ukraine',
+  city: 'Kyiv',
+  creditCard: '4444444444444444',
+  month: '11',
+  year: '12'
+};
