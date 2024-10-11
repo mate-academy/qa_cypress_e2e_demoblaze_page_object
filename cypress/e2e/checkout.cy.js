27cca33b99ad8c854eaa6d2badcac9eb7ffef4d6
Oleg <_email@example.com>
2024-10-11 17:29:17 +0300

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,58 @@
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+import HomeAndCataloguePageObject from '../support/pages/homeСatalogue.pageObject';
+import ContactFormPageObject from '../support/pages/contactForm.pageObject';
+import { faker } from '@faker-js/faker';  // Обновленный импорт
 
+const homePage = new HomeAndCataloguePageObject();
+const contactForm = new ContactFormPageObject();
+const testData = {
+  product: 'Sony vaio i7',
+  category: 'Laptops',
+  alertMessage: 'Product added',
+  name: faker.name.firstName(),
+  country: faker.address.country(),
+  city: faker.address.city(),
+  card: faker.finance.creditCardNumber(),
+  month: '12',
+  year: '2024',
+  successMessage: 'Thanks for the message!!'
+};
+
+describe('Demoblaze Checkout Flow', () => {
+  before(() => {
+    homePage.visit(); // Переход на домашнюю страницу
   });
 
-  it('', () => {
+  it('should complete checkout flow', () => {
 
+    homePage.clickOnCategory(testData.category);
+
+    homePage.clickOnProduct(testData.product);
+
+    homePage.addToCart();
+    homePage.assertAllert(testData.alertMessage);
+
+
+    cy.contains('Cart').click();
+    cy.contains(testData.product).should('be.visible');
+    cy.contains('Place Order').click();
+
+
+    cy.get('#name').type(testData.name);
+    cy.get('#country').type(testData.country);
+    cy.get('#city').type(testData.city);
+    cy.get('#card').type(testData.card);
+    cy.get('#month').type(testData.month);
+    cy.get('#year').type(testData.year);
+
+
+    cy.contains('Purchase').click();
+
+    cy.get('.sweet-alert').should('contain', testData.name);
+    cy.get('.sweet-alert').should('contain', testData.card);
+
+    cy.contains('OK').click();
   });
 });
+
