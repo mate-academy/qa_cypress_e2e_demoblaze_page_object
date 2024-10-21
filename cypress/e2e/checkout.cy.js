d94eeaff8b6c2d63f346f016c36c81a7909ffcbd
Vita <st.vitalina666@gmail.com>
2024-10-21 16:17:31 +0300

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,51 @@
+import PlaceOnOrderPageObject
+  from '../support/pages/PlaceAnOrder.pageObject.js';
+import PageObject from '../support/PageObject.js';
+import HomeAndCataloguePageObject
+  from '../support/pages/homeCatalogue.pageObject';
+const { faker } = require('@faker-js/faker');
+
+const ProductPage = new PlaceOnOrderPageObject();
+const homePage = new HomeAndCataloguePageObject();
+const allertAssertion = new PageObject();
+const cartPage = new PlaceOnOrderPageObject();
+const submitOrderFrom = new PlaceOnOrderPageObject();
+
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+const testData = {
+  email: faker.internet.email(),
+  name: faker.person.firstName(),
+  message: faker.word.words({ count: { min: 5, max: 10 } }),
+  successMessage: 'Thank you for your purchase!',
+  country: faker.location.country(),
+  city: faker.location.city(),
+  creditCard: faker.number.int(),
+  month: faker.date.month(),
+  year: faker.number.int({ min: 1990, max: 2013 })
+};
 
+describe('Demoblaze (POM basics) Workflow', () => {
+  before(() => {
+    homePage.visit();
   });
 
-  it('', () => {
-
+  it('should provide an ability to place an order', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    ProductPage.clickOnAddToCardBtn();
+    allertAssertion.assertAllert('Product added');
+    homePage.clickOnLink('Cart');
+    cartPage.ensureProductInCart('Sony vaio i7');
+    cartPage.openPlaceOrderForm();
+    submitOrderFrom.typeName(testData.name);
+    submitOrderFrom.typeCoutry(testData.country);
+    submitOrderFrom.typeCity(testData.city);
+    submitOrderFrom.typeCreditCard(testData.creditCard);
+    submitOrderFrom.typeMonth(testData.month);
+    submitOrderFrom.typeYear(testData.year);
+    submitOrderFrom.clickOnPurchaseBtn();
+    submitOrderFrom.ensureSuccessAllert(testData.successMessage);
+    submitOrderFrom.sumbitSucessModal();
   });
 });
---
+++ cypress/support/pages/PlaceAnOrder.pageObject.js
@@ -0,0 +1,123 @@
+import PageObject from '../PageObject';
+
+class PlaceOnOrderPageObject extends PageObject {
+  url = '/index.html';
+
+  get emailField() {
+    return cy.get('#email');
+  }
+
+  get nameField() {
+    return cy.get('#name');
+  }
+
+  get countryField() {
+    return cy.get('#country');
+  }
+
+  get cityField() {
+    return cy.get('#city');
+  }
+
+  get messageAllert() {
+    return cy.get('.sweet-alert');
+  }
+
+  get creditCardField() {
+    return cy.get('#card');
+  }
+
+  get sendMessageBtn() {
+    return cy.contains('.btn', 'Send message');
+  }
+
+  get BtnForAddedProduct() {
+    return cy.get('[onclick="addToCart(9)"]');
+  }
+
+  get BtnForConfirmOrder() {
+    return cy.contains('[type=button]', 'Place Order');
+  }
+
+  get confirmAllertBtn() {
+    return cy.get('.confirm.btn.btn-lg.btn-primary');
+  }
+
+  get monthFiedl() {
+    return cy.get('#month');
+  }
+
+  get yearFiedl() {
+    return cy.get('#year');
+  }
+
+  get productInCart() {
+    return cy.get('.success');
+  }
+
+  get purchaseBtn() {
+    return cy.get('[onclick="purchaseOrder()"]');
+  }
+
+  typeEmail(email) {
+    this.emailField.type(email);
+  }
+
+  typeCity(city) {
+    this.cityField.type(city);
+  }
+
+  typeName(name) {
+    this.nameField.type(name);
+  }
+
+  typeCoutry(country) {
+    this.countryField.type(country);
+  }
+
+  typeCreditCard(creditCard) {
+    this.creditCardField.type(creditCard);
+  }
+
+  typeMonth(month) {
+    this.monthFiedl.type(month);
+  }
+
+  typeYear(year) {
+    this.yearFiedl.type(year);
+  }
+
+  typeMessage(message) {
+    this.messageField.type(message);
+  }
+
+  clickOnSendMessageBtn() {
+    this.sendMessageBtn.click();
+  }
+
+  clickOnAddToCardBtn() {
+    this.BtnForAddedProduct.click();
+  }
+
+  ensureProductInCart(product) {
+    this.productInCart.should('contain', product);
+  }
+
+  openPlaceOrderForm() {
+    this.BtnForConfirmOrder.should('exist', { delay: 15000 }).click();
+  }
+
+  clickOnPurchaseBtn() {
+    this.purchaseBtn.click();
+  }
+
+  ensureSuccessAllert(successMessage) {
+    this.messageAllert.should('contain', successMessage);
+  }
+
+  sumbitSucessModal() {
+    this.confirmAllertBtn.click();
+  }
+}
+
+export default PlaceOnOrderPageObject;
