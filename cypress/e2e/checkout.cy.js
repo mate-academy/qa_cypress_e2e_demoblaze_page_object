0429f102086ef3ec89936b385644c0ddf4bc86fe
luckezQA <labchuk2007@gmail.com>
2024-10-21 15:53:51 +0300

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,39 @@
-/// <reference types='cypress' />
+import { faker } from '@faker-js/faker';
+import HomeAndCataloguePageObject
+  from '../support/pages/homeCatalogue.pageObject';
 
-describe('', () => {
+const homeCatalogue = new HomeAndCataloguePageObject();
+const name = faker.person.firstName();
+const country = faker.location.country();
+const city = faker.location.city();
+const card = faker.finance.creditCardNumber();
+const month = faker.date.month();
+const year = new Date().getFullYear();
+
+describe('Demoblaze', () => {
   before(() => {
-
+    homeCatalogue.visit();
   });
 
-  it('', () => {
+  it('should add new product in cart', () => {
+    homeCatalogue.clickOnCategory('Laptops');
+    homeCatalogue.clickOnProduct('Sony vaio i7');
+    homeCatalogue.clickOnButton('Add to cart');
 
+    homeCatalogue.verifyAddingToCart('Product added');
+
+    homeCatalogue.clickOnLink('Cart');
+    homeCatalogue.verifyProductinCart('Sony vaio i7');
+
+    homeCatalogue.clickOnButton('Place Order');
+    homeCatalogue.typeName(name);
+    homeCatalogue.typeCountry(country);
+    homeCatalogue.typeCity(city);
+    homeCatalogue.typeCard(card);
+    homeCatalogue.typeMonth(month);
+    homeCatalogue.typeYear(year);
+    homeCatalogue.clickOnPurchaseBtn();
+    homeCatalogue.verifyPurchasing(name, card);
+    homeCatalogue.clickOnOkBtn();
   });
 });
