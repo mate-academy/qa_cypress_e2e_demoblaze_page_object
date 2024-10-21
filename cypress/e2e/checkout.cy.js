4c1e9afba940927f5ce88401d3693cbf72fe7185
nevermore <gritsanowo@gmail.com>
2024-10-21 12:43:47 +0300

    solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,30 @@
 /// <reference types='cypress' />
 
+import CheckoutPageObject from '../support/pages/checkout.pageObject';
+
+const checkoutPage = new CheckoutPageObject();
+
 describe('', () => {
   before(() => {
-
+    checkoutPage.visit();
   });
 
-  it('', () => {
-
+  it('should test checkout', () => {
+    checkoutPage.laptopsFieldClick();
+    checkoutPage.laptopItemClick();
+    checkoutPage.addToCartBtnClick();
+    checkoutPage.alertMessage();
+    checkoutPage.cartBtnClick();
+    checkoutPage.assertCart();
+    checkoutPage.placeOrderBtnClick();
+    checkoutPage.fillNameField('Test');
+    checkoutPage.fillCountryField('Romania');
+    checkoutPage.fillCityField('Bucharest');
+    checkoutPage.fillCreditCardField('123213123');
+    checkoutPage.fillMonthField('12');
+    checkoutPage.fillYearField('2022');
+    checkoutPage.purchaseBtnClick();
+    checkoutPage.assertSuccessModal();
+    checkoutPage.confirmModal();
   });
 });
