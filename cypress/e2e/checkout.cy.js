739ff8a6ac9837bcb3a39679005f5ad75594f7e2
Mariana Khrustina <khrystinamariana@gmail.com>
2024-10-22 01:40:18 +0400

    Solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,43 @@
-/// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+describe('Checkout Test', () => {
+  it('should complete a purchase and verify order details', () => {
+    // Дії для додавання продукту до кошика
+    cy.visit('https://www.demoblaze.com/index.html');
+    cy.contains('Laptops').click();
+    cy.contains('Sony vaio i7').click();
+    cy.contains('Add to cart').click();
 
-  });
+    // Перевірка повідомлення
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Product added');
+    });
 
-  it('', () => {
+    // Перехід до кошика
+    cy.get('#cartur').click();
 
+    // Перевірка, що продукт в кошику
+    cy.get('tr').within(() => {
+      cy.get('td').contains('Sony vaio i7').should('be.visible');
+    });
+
+    // Клік на "Place order"
+    cy.contains('Place Order').click();
+
+    // Заповнення форми замовлення
+    cy.get('#name').type('mariana khrys');
+    cy.get('#country').type('Canada');
+    cy.get('#city').type('Ottawa');
+    cy.get('#card').type('1234455666778');
+    cy.get('#month').type('April');
+    cy.get('#year').type('2024');
+    cy.contains('Purchase').click();
+
+    // Перевірка, що модальне вікно містить правильні дані
+    cy.get('.lead').should('contain.text', 'mariana khr'); // Перевіряємо скорочене ім'я
+    cy.get('.lead').should('contain.text', '790 USD');
+    cy.get('.lead').should('contain.text', '1234455666778');
+
+    // Клік на "ОК" для завершення покупки
+    cy.get('button.confirm.btn.btn-lg.btn-primary').click();
   });
 });
