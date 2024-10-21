5cce0f9dc14a471cad1e85e1630a2c1d5956bd28
Sofi Mamzer <sofiiamamzer06@gmail.com>
2024-10-21 17:54:07 +0300

    DemoBlaze e2e

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,52 @@
+
+import HomeAndCataloguePageObject
+  from '../support/pages/homeCatalogue.pageObject';
+import ProductPageObject
+  from '../support/pages/productPageObject';
+import CartPage
+  from '../support/pages/cartPageObject';
+
+const { faker } = require('@faker-js/faker');
+
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+const homePage = new HomeAndCataloguePageObject();
+const productPage = new ProductPageObject();
+const cartPage = new CartPage();
 
+const catagoryName = 'Laptops';
+const productName = 'Sony vaio i7';
+const orderName = faker.person.firstName();
+const userCountry = faker.location.country();
+const creditCard = faker.finance.creditCardNumber();
+
+const month = faker.number.int({ min: 1, max: 12 });
+const city = faker.location.city();
+const year = faker.number.int({ min: 1900, max: 2024 });
+
+describe('Demoblaze Checkout Flow', () => {
+  before(() => {
+    homePage.visit();
   });
 
-  it('', () => {
+  it('should complete the checkout process', () => {
+    homePage.clickOnCategory(catagoryName);
+    homePage.clickOnProduct(productName);
 
+    productPage.addToCartBtn.click();
+    productPage.assertAllert('Product added');
+
+    cartPage.visitCart();
+    cartPage.assertProductInCart(productName);
+
+    cartPage.placeOrderBtn();
+    cartPage
+      .fillOrderFields(orderName, userCountry, creditCard, month, city, year);
+
+    cartPage.confirmOrder();
+
+    cartPage.assertEnteredDataOnModal(orderName, creditCard);
+
+    cartPage.clickOk();
   });
 });
