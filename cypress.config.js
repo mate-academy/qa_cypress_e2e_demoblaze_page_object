70e6e84c39d9223c3f4cb26c9c23e74e2f204457
Anton Zhyhanovskyi <zhyganovskiy7@gmail.com>
2024-10-21 14:15:03 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress');
+const faker = require('@faker-js/faker').faker;
 
 module.exports = defineConfig({
   e2e: {
@@ -8,12 +9,17 @@ module.exports = defineConfig({
         generateUser() {
           const randomNumber = Math.ceil(Math.random(1000) * 1000);
           return {
-            username: faker.name.firstName() + `${randomNumber}`,
+            username: faker.person.firstName() + `${randomNumber}`,
             email: 'test' + `${randomNumber}` + '@mail.com',
-            password: 'Password12345!'
+            password: 'Password12345!',
+            country: faker.location.country(),
+            city: faker.location.city(),
+            creditCard: faker.finance.creditCardNumber(),
+            month: '07',
+            year: '2026'
           };
         }
-      })
+      });
     }
   }
 });
--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,46 @@
 /// <reference types='cypress' />
+import PageObject from '../support/PageObject';
+import OrderFormPageObject from '../support/pages/orderForm.pageObject';
+import HomeAndCataloguePageObject
+  from '../support/pages/homeCatalogue.pageObject';
+import { faker } from '@faker-js/faker';
+
+const orderForm = new OrderFormPageObject();
+const homePage = new HomeAndCataloguePageObject();
+const allertMessage = new PageObject();
+
+const testData = {
+  name: faker.person.firstName(),
+  country: faker.location.country(),
+  city: faker.location.city(),
+  creditCard: faker.finance.creditCardNumber(),
+  month: '07',
+  year: '2026',
+  successMessage: 'Thanks you for your purchase!'
+};
 
 describe('', () => {
   before(() => {
-
+    homePage.visit();
   });
 
-  it('', () => {
+  it('should provide the ability to add product to the cart', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    cy.get('[onclick="addToCart(9)"]').click();
+    allertMessage.assertAllert('Product added');
+    homePage.clickOnLink('Cart');
+    cy.contains('td', 'Sony vaio i7').should('be.visible');
+    cy.get('.col-lg-1 > .btn').click();
+    orderForm.typeName(testData.name);
+    orderForm.typeCountry(testData.country);
+    orderForm.typeCity(testData.city);
+    orderForm.typeCard(testData.creditCard);
+    orderForm.typeMonth(testData.month);
+    orderForm.typeYear(testData.year);
+    orderForm.clickOnPurchaseBtn();
 
+    orderForm.assertAllert(testData.successMessage);
+    cy.get('.confirm').click();
   });
 });
